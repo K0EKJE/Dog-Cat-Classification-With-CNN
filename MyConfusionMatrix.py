4555442ae666cca0b20de1a25ec27b01e15b5c5b
Haoran RAIN <95905185+K0EKJE@users.noreply.github.com>
2021-12-14 16:36:53 -0800

    Add files via upload
    This file stores a function to visualize model's performance on validation set

---
+++ MyConfusionMatrix.py
@@ -0,0 +1,19 @@
+from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
+
+def my_confusion_matrix(model):
+    
+    predict = model.predict_generator(validation_generator,
+                                      steps=np.ceil(val_set.shape[0] / 64))
+
+    val_set['predict'] = np.argmax(predict, axis=-1)
+    labels = dict((v,k) for k,v in train_generator.class_indices.items())
+    val_set['cat/dog'] = val_set['predict'].map(labels)
+
+    fig, ax = plt.subplots(figsize = (9, 6))
+
+    cm = confusion_matrix(val_set["categories"], val_set["cat/dog"])
+    disp = ConfusionMatrixDisplay(confusion_matrix = cm, display_labels = ["cat", "dog"])
+    disp.plot(cmap = plt.cm.Blues, ax = ax)
+
+    ax.set_title("Validation Set")
+    plt.show()
